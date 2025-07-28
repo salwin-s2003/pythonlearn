4a43163510b8ac1b8a834133490bf175d2f6b7b5
salwin-s2003 <salwinsolomon12345@gmail.com>
2025-07-29 00:11:41 +0530

    Useful additional information

--- Some_Useful_info.py
+++ Some_Useful_info.py
@@ -10,3 +10,17 @@ print("The address of b :",id(b)," and the address of c :",id(c))
 
 #whenever u wanted to know any type of the variable in python
 print(type(a))
+"""data types in python
+int, float, complex, bool, string, list, tuple, set, dict(Mapping), none, Numeric,range
+Numeric is a parent class of int and float . numeric is not a data type in python. numeric has int,float,bool,complex as its child classes.
+sequence is a parent class of list, tuple, range, string. sequence is not a data type in python. sequence has list, tuple, range, string as its child classes.
+InPyhon there is no char data type.
+"""
+# to convert float to int 
+a = 10.5
+b = int(a)
+type(b) 
+""" In python, the 1 is true and 0 is false"""
+print(int(True))
+print(int(False))
+
